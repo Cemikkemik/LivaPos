d043385ff1aad7bd0e86e7416d2b3a771a93868f
ebencarljo <blair_jersyer@hotmail.fr>
2016-06-24 02:37:13 +0100

    Update Session
    Update Session

--- application/config/config.php
+++ application/config/config.php
@@ -359,7 +359,7 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 60*60*3;
 $config['sess_save_path'] = null;

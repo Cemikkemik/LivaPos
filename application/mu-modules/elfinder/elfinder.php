3b1e32b5679357a7920785096f9503ada6aef204
Blair Jersyer <carlosjohnsonluv2004@gmail.com>
2017-09-04 23:15:30 +0100

    Short Fixes
    - AUTHOR Name for orders
    - Order processing form digital orders
    - Display hidden button for mobile
    - New permission for NSAM
    - Elfinder short update + migrate

--- application/mu-modules/elfinder/elfinder.php
+++ application/mu-modules/elfinder/elfinder.php
@@ -12,6 +12,33 @@ class elFinder extends CI_Model{
 
         $this->events->add_action( 'load_dashboard', array( $this, 'load_dashboard' ) );
         $this->events->add_filter( 'admin_menus', array( $this, 'admin_menus' ), 15 );
+        $this->events->add_action( 'do_enable_module', [ $this, 'enable_module' ] );
+        $this->events->add_action( 'tendoo_settings_final_config', [ $this, 'final_config' ] );
+    }
+
+    /**
+     * Final Config
+     * @return config
+    **/
+
+    public function final_config()
+    {
+        $this->aauth        =    $this->users->auth;
+        $this->aauth->create_perm( 'view_file_manager',    __( 'File Manager Access', 'nexo'),            __('Let the use have access to the file manager.', 'nexo'));
+        $this->aauth->allow_group( 'master', 'view_file_manager');
+    }
+
+    /**
+     * Enable module
+     * @return void
+    **/
+
+    public function enable_module( $namespace ) 
+    {
+        if( $namespace == 'elfinder' && get_option( 'elfinder_installed' ) == null ) {
+            $this->aauth->create_perm( 'view_file_manager',    __( 'File Manager Access', 'nexo'),            __('Let the use have access to the file manager.', 'nexo'));
+            $this->optiosn->set( 'view_file_manager', 'true', true );
+        }
     }
 
     /**
@@ -25,7 +52,7 @@ class elFinder extends CI_Model{
     public function admin_menus( $menus )
     {
         $backup     =   $menus;
-        if( User::can( 'edit_options' ) ) :
+        if( User::can( 'view_file_manager' ) ) :
             $menus  =   array_insert_before( 'settings', $menus, 'elfinder', array(
                 array(
                     'title' =>  __( 'File Manager', 'elfinder' ),
@@ -46,7 +73,7 @@ class elFinder extends CI_Model{
 
     public function load_dashboard()
     {
-        if( User::can( 'edit_options' ) ) :
+        if( User::can( 'view_file_manager' ) ) :
             $this->Gui->register_page_object( 'elfinder', new elFinderController );
         endif;
     }

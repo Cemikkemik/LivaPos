6410969f66de662d7772ca50f73e446a2b10d7a3
Blair Jersyer <carlosjohnsonluv2004@gmail.com>
2017-09-04 12:18:36 +0100

    Fix customer Translation
    Fix customer Translation

--- application/modules/nexo/views/customers/main-template.php
+++ application/modules/nexo/views/customers/main-template.php
@@ -4,7 +4,7 @@
             <div class="input-group input-group-lg name-input-group">
                 <input required type="text" name="name" ng-model="model[ 'name' ]" class="form-control"placeholder="<?php echo __( 'Customer Name', 'nexo' );?>">
                 <span class="input-group-btn customer-save-btn">
-                    <button type="submit" class="btn btn-default"><?php echo __( 'Sauvegarder', 'nexo' );?></button>
+                    <button type="submit" class="btn btn-default"><?php echo __( 'Enregistrer', 'nexo' );?></button>
                 </span>
             </div>
             <p class="help-block" ng-show="( form.name.$error.required && form.name.$touched ) || ( form.name.$error.required && form.$submitted )">

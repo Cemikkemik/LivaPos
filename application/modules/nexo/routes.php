3da1079888a11d51dfaaf0205ac71d646d1c0b72
Blair Jersyer <carlosjohnsonluv2004@gmail.com>
2017-11-29 23:34:41 +0100

    Update Nexo Routes

--- application/modules/nexo/routes.php
+++ application/modules/nexo/routes.php
@@ -3,18 +3,11 @@ global $Routes;
 
 include( dirname( __FILE__ ) . '/routes-array.php' );
 
-<<<<<<< HEAD
-$Route->get( 'nexo/templates/customers-main', 'NexoTemplateController@customers_main' );
-$Route->get( 'nexo/templates/customers-form', 'NexoTemplateController@customers_form' );
-$Route->get( 'nexo/templates/shippings', 'NexoTemplateController@shippings' );
-$Route->match([ 'get', 'post' ], 'stores/{id}/{any}', 'NexoStoreController@stores' )->where([ 
-=======
 $Routes->get( 'nexo/templates/customers-main', 'NexoTemplateController@customers_main' );
 $Routes->get( 'nexo/templates/customers-form', 'NexoTemplateController@customers_form' );
 $Routes->get( 'nexo/templates/shippings', 'NexoTemplateController@shippings' );
 $Routes->get( 'nexo/template/{name}', 'NexoTemplateController@template' );
 $Routes->match([ 'get', 'post' ], 'stores/{id}/{any}', 'NexoStoreController@stores' )->where([ 
->>>>>>> 652b558... Update
     'id'      => '[0-9]+', 
     'any'     =>   '.*' 
 ]);

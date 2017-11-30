2d31e7af2b6571664a76aa6ccbead6d961d94ae5
Blair Jersyer <carlosjohnsonluv2004@gmail.com>
2017-12-01 00:59:24 +0100

    Update
    - Remove Updater

--- application/modules/nexo-updater/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-global $Routes;
-
-$Routes->match([ 'get', 'post' ], '/nexo/licence', 'NexoUpdaterLicence@index' )->name( 'nexo.licence' );
-$Routes->match([ 'get', 'post' ], '/nexo/licence/activated', 'NexoUpdaterLicence@activated' )->name( 'nexo.licence' );
